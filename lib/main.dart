3ed8ca2976ef30df3d0422ade10779b51b4e55f5
Ducasx <alessandro04dl@gmail.com>
2022-11-07 16:49:28 +0100

    copied Marcoigorr repo

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,20 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:MyApp/landing/landing_page.dart';
+import 'package:MyApp/theme.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  static const String _title = 'De Lucas App';
-  static const Color _bgColor = Color.fromARGB(255, 193, 219, 230);
-  static const Color _bgDarkerColor = Color.fromARGB(255, 95, 181, 218);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Root of application
     return MaterialApp(
+      home: LandingPage(),
+      theme: appTheme,
       debugShowCheckedModeBanner: false,
-      title: _title,
-      theme: ThemeData(scaffoldBackgroundColor: _bgColor),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Progetto Flutter di De Luca'),
-          backgroundColor: _bgDarkerColor,
-          centerTitle: true,
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(50),
-          children: <Widget>[
-            Container(
-              height: 50,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry A')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[500],
-              child: const Center(child: Text('Entry B')),
-            ),
-            Container(
-              height: 50,
-              color: Colors.amber[100],
-              child: const Center(child: Text('Entry C')),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
