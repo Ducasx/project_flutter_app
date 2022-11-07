19c4678ee89a426ef77473fe13a6d7ede7f9f89f
Ducasx <alessandro04dl@gmail.com>
2022-11-07 14:24:14 +0100

    changed main page content to ListView

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   static const String _title = 'De Lucas App';
-  static const Color _bgColor = Color.fromARGB(255, 99, 84, 139);
-  static const Color _bgDarkerColor = Color.fromARGB(255, 66, 56, 92);
+  static const Color _bgColor = Color.fromARGB(255, 193, 219, 230);
+  static const Color _bgDarkerColor = Color.fromARGB(255, 95, 181, 218);
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +21,25 @@ class MyApp extends StatelessWidget {
           backgroundColor: _bgDarkerColor,
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Hello world!'),
+        body: ListView(
+          padding: const EdgeInsets.all(50),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ],
         ),
       ),
     );
